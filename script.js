81c63a64901002b3f800966a93acfba0bc41a2fc
Nutchaya <nutchaya.kon@student.mahidol.ac.th>
2018-11-27 20:58:40 +0700

    my mistake

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const API_KEY = "AIzaSyBZu7QDoRxul0IJ64Mpb1ew2LKMp5zs4gs";
+<script>
+  (function() {
+    var cx = '006915378234751030454:0aaxsuvrxzi';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
