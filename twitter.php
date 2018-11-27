496dc64607acb8dfc3beff741ee1255278e14a42
pppetonggg <paweennat.foi@student.mahidol.ac.th>
2018-11-28 00:38:11 +0700

    all twitter

--- twitter.php
+++ twitter.php
@@ -8,23 +8,24 @@
     define("AccessToken","1066363490539995136-E0lD20m4hDZoIYFab3xswSZW0NC0yy");
     define("AccessTokenSecret","7gFEJcbkalXk8OnoZ4IrcXWdaZrIdUk6u6bMpbMGL1gQh");
 
+
     function show($result){
         foreach($result->statuses as $key => $value){
             echo "
             <div class=\"container\">
-                <div class=\"col-md-12 col-sm-12 col-xs-12\">
-                <table class=\"table\">
-                <thead>
-			    <td rowspan=\"2\"><img src=\"{$value->user->profile_image_url}\" style=\"margin-top:20px;\" alt=\"Avatar\" width=\"48px\" height=\"48px\" class=\"rounded-circle\"></td>
-			    <td><h4><b>{$value->user->name}</b></h4></td>
-		        </tr>
-		        <tr>
-			    <td><p style=\"margin-top:5px;\">{$value->text}</p></td>
-		        </tr>
-                </thead>
-               
-                </div>
-            </div>
+                
+            <table class=\"table\" border=\"1\">
+            <thead>
+            <td rowspan=\"2\"><img src=\"{$value->user->profile_image_url}\"></td>
+            <td><h4>{$value->user->name}</h4></td>
+            </tr>
+            <tr>
+            <td><p>{$value->text}</p></td>
+            </tr>
+            </thead>
+           
+           
+        </div>
             ";
     }
 
@@ -41,6 +42,7 @@
           );
 
           $result = search($needed_data);
+          show($result);
     }
 
 ?>
